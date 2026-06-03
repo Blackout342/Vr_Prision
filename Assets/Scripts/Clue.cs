d4724ba3c30e3476f3c4c902752154dadfadc610
Didi-121 <edidier11@outlook.com>
2026-06-03 01:01:57 -0600

    fix1

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -7,11 +7,7 @@ public class Clue : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-<<<<<<< HEAD
-        SFXManager.Instance.globalAudioSource.PlayOneShot(SFXManager.Instance.clue, 0.8f);
-=======
         SFXManager.Instance.PlaySound3D(SFXManager.Instance.clue, transform.position, 100f, 15f, true);
->>>>>>> c44ec47 (Second audio source)
     }
 
     void Start()
